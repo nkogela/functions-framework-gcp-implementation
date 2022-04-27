9cd63b7ceee912b74853773d921cea6df23893ec
bichi08 <33770822+bichi08@users.noreply.github.com>
2022-04-27 21:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,12 @@ import urllib3
 import json
 import traceback
 
-import os
-import flask
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+# import os
+# import flask
+# from slack_bolt import App
+# from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-slack_webhook: str = 'https://hooks.slack.com/services/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'
-xapp_token: str = 'xapp-1-A03CQEQH06Q-3430158740130-518e3082f9a09cbc17ee8c421d5c8452ab51014deb51f5fced6a4090507a3e7d'
-
-
-# Initializes app with bot token and socket mode handler
-# app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
-# SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-#
-# app = App(
-#     token=os.environ.get("SLACK_BOT_TOKEN"),
-#     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-# )
+slack_webhook: str = 'https://hooks.slack.com/services/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'
 
 
 @functions_framework.http
@@ -29,6 +18,8 @@ def notify_to_slack(request):
     :param request: Most Likely a json as a string
                     request (flask.Request): The request object.
                     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+
+                    slack webhook can be set through request parameters
     :return: success String as per need
     """
 
@@ -37,6 +28,8 @@ def notify_to_slack(request):
     request_args = request.args
     if request_args and "data" in request_args:
         message_payload = request_args["data"]
+        # Converting to String
+        message_payload = str(message_payload)
 
     notify_slack(message_payload)
     return 'Success'
@@ -44,28 +37,29 @@ def notify_to_slack(request):
 
 # Send Slack notification based on the given message
 def notify_slack(message):
-    # try:
-    #     slack_message = {'text': message}
-    #     print(slack_message)
-    #     http = urllib3.PoolManager()
-    #     response = http.request('POST',
-    #                             webhook_url,
-    #                             body=json.dumps(slack_message),
-    #                             headers={'Content-Type': 'application/json'},
-    #                             retries=False)
-    #
-    #     print("trying to Push to Slack...")
-    #     print(response.status)
-    #     if response.status != 200:
-    #         print("Trying Alternate method")
-    #         import os
-    #         cmd = 'curl -X POST -H \"Content-type: application/json\" --data \"{\\\"text\\\":\\\"Hello, World!\\\"}\"' + " " + webhook_url
-    #         print(cmd)
-    #         os.system(cmd)
-    #     else:
-    #         print("Message Sent to Slack...")
-    # except Exception as e:
-    #     traceback.print_exc()
+    try:
+        slack_message = {'text': message}
+        print(slack_message)
+        http = urllib3.PoolManager()
+        response = http.request('POST',
+                                slack_webhook,
+                                body=json.dumps(slack_message),
+                                headers={'Content-Type': 'application/json'},
+                                retries=False)
+
+        print("trying to Push to Slack...")
+        print(response.status)
+        if response.status != 200:
+            print("Trying Alternate method")
+            import os
+            cmd = 'curl -X POST -H \"Content-type: application/json\" --data \"{\\\"text\\\":\\\"Hello, World!\\\"}\"' + " " + slack_webhook
+            print(cmd)
+            os.system(cmd)
+        else:
+            print("Message Sent to Slack...")
+            print(f"Message sent to Slack : {message}")
+    except ConnectionError as e:
+        traceback.print_exc()
     return True
 
 
@@ -79,4 +73,3 @@ functions_framework --target=notify_to_slack --host=<The host on which the Funct
 """
 
 # Command to Run - curl -X POST http://127.0.0.1:8080 -H "Content-Type: application/json" -d \"{\\\"slack_info\\\":{\\\"enable": \\\"False\\\", \\\"channel_name\\\":\\\"#paidmedia_notification\\\"}}\"
-
